01ab10e85223b276a52b97688054b984f37c92c0
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-08-21 17:54:45 +0430

    working on about section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import About from './components/about/About';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
 
       <main>
         <Home/>
+        <About/>
       </main>
     </>
   )
