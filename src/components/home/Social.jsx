733a456f098a08db78097e01182bc9625dc89730
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-08-21 14:43:56 +0430

    working on homepage

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="linkedin.com/in/ahmaddonishyar-527937197/" className='home__social-icon' target='_blank'>
+            <i className="uil uil-linkedin"></i>
+        </a>
+        <a href="https://github.com/Donishyar" className='home__social-icon' target='_blank'>
+            <i className="uil uil-github-alt"></i>
+        </a>
+        <a href="https://twitter.com/ahmaddonishyar" className='home__social-icon' target='_blank'>
+            <i className="uil uil-twitter"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
