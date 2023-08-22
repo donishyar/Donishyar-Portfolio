78b85b7ffec5c8ece514b6da0cbc061687301e1a
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-08-22 17:58:42 +0430

    working on services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./services.css";
 
 const Services = () => {
+    const [toggleState, setToggleState] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
   return (
     <section className="services section" id="services">
 
@@ -12,13 +18,15 @@ const Services = () => {
                     <h3 className="services__title">Frontend <br />Developer</h3>
                 </div>
 
-                <span className="services__button">View More
+                <span className="services__button" onClick={() => {
+                    toggleTab(1)
+                }}>View More
                 <i className="uil uil-arrow-right services__button-icon">
                 </i></span>
 
-                <div className="services__modal">
+                <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
 
                     <h3 className="services__modal-title">Frontend Developer</h3>
                     <p className="services__modal-description">
@@ -47,20 +55,6 @@ const Services = () => {
                              Responsive webpages for 99.9% Devices.
                             </p>
                         </li>
-
-                        <li className="services__modal-service">
-                            <i className="uil uil-check-circle services__modal-icon"></i>
-                            <p className="services__modal-info">
-                             Design the most memorable LOGOs;
-                            </p>
-                        </li>
-
-                        <li className="services__modal-service">
-                            <i className="uil uil-check-circle services__modal-icon"></i>
-                            <p className="services__modal-info">
-                             Design creative color themes
-                            </p>
-                        </li>
                     </ul>
 
                     </div>
@@ -73,13 +67,15 @@ const Services = () => {
                     <h3 className="services__title">Graphic<br />Designer</h3>
                 </div>
 
-                <span className="services__button">View More
+                <span  className="services__button" onClick={() => 
+                    toggleTab(2)
+                } >View More
                 <i className="uil uil-arrow-right services__button-icon">
                 </i></span>
 
-                <div className="services__modal">
+                <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i onClick={()=> { toggleTab(0)}} className="uil uil-times services__modal-close"></i>
 
                     <h3 className="services__modal-title">Graphic Designer</h3>
                     <p className="services__modal-description">
@@ -88,31 +84,18 @@ const Services = () => {
                     </p>
 
                     <ul className="services__modal-services grid">
-                        <li className="services__modal-service">
+
+                    <li className="services__modal-service">
                             <i className="uil uil-check-circle services__modal-icon"></i>
                             <p className="services__modal-info">
-                                I develop the most wonderful UIs.
+                             Make a brand from scratch
                             </p>
                         </li>
 
                         <li className="services__modal-service">
                             <i className="uil uil-check-circle services__modal-icon"></i>
                             <p className="services__modal-info">
-                                Component base UI system.
-                            </p>
-                        </li>
-
-                        <li className="services__modal-service">
-                            <i className="uil uil-check-circle services__modal-icon"></i>
-                            <p className="services__modal-info">
-                             Responsive webpages for 99.9% Devices.
-                            </p>
-                        </li>
-
-                        <li className="services__modal-service">
-                            <i className="uil uil-check-circle services__modal-icon"></i>
-                            <p className="services__modal-info">
-                             Design the most memorable LOGOs;
+                             Design the most memorable LOGOs
                             </p>
                         </li>
 
