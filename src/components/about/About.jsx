3d5fda163faba7ac499d4eaee5016eae56f866b1
donishyar <ahmadshahdonishyar@gmail.com>
2023-08-25 18:05:52 +0430

    adding my photo and cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/Ahmad's Resume With Photo for European recruiters.pdf";
 import Info from './Info';
 
 const About = () => {
